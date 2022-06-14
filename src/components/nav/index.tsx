87f32089242f8f79fdcb1bb47df2af19faa0687d
Thiago Almeida <85900124+thiagoalmeidas@users.noreply.github.com>
2022-06-14 10:26:00 -0300

    background gradient

--- src/components/nav/index.tsx
+++ src/components/nav/index.tsx
@@ -5,7 +5,7 @@ import Logo from '../logo'
 function Nav() {
 
     return (
-        <div className="flex-fill bg-info bg-dark" style={{ background: 'linear-gradient(13deg, #dc3545 0%, rgb(253 45 150) 95%)'}}>
+        <div className="flex-fill bg-info bg-dark" style={{ background: 'linear-gradient(13deg, rgb(53 83 220) 0%, rgb(112 45 253) 95%)'}}>
             <nav className="container p-3 d-flex justify-content-between align-items-center">
                 <Logo />
                 <Menu />
